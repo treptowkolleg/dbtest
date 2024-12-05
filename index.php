2744a1e87c3352d214a6e000f12bd018724e49f6
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-12-05 13:35:33 +0100

    update

--- index.php
+++ index.php
@@ -3,27 +3,12 @@
 
 use App\Entity\Teacher;
 use App\Repository\TeacherRepository;
-use TreptowKolleg\ORM\Model\Database;
+use TreptowKolleg\ORM\Model\EntityManager;
 use TreptowKolleg\ORM\Model\Repository;
 
 require 'vendor/autoload.php';
 
-################
-# Ohne Mapping #
-################
 
-$db = (new Database())->getConnection();
-$statement = $db->prepare("SELECT id, firstname, lastname FROM teacher WHERE firstname = :firstname");
-$statement->bindValue(':firstname', 'Klaus');
-$statement->execute();
-$result = $statement->fetch(PDO::FETCH_ASSOC);
-if($result) {
-    echo "{$result["firstname"]} {$result["lastname"]} \n";
-}
-
-###############
-# Mit Mapping #
-###############
 
 // Mapping auf Teacher-Klasse
 $repository = new Repository(Teacher::class);
@@ -32,12 +17,18 @@ $repository = new Repository(Teacher::class);
 // 'firstname' = 'Klaus' und erzeugt neues Teacher-Objekt:
 $teacher = $repository->findOneBy(['firstname' => 'Klaus']);
 echo $teacher ?: "Kein Datensatz gefunden";
+echo PHP_EOL;
+echo PHP_EOL;
 
 // benutzerdefiniertes Teacher-Repository
 $teacherRepository = new TeacherRepository();
-$teacher2 = $teacherRepository->findOneLike(['firstname' => 'en', 'lastname' => 'agn']);
-
-
-echo PHP_EOL;
-echo $teacher2 ?: "Kein Datensatz gefunden";
-echo PHP_EOL;
+$teacher2 = $teacherRepository->findOneLike(['firstname' => 'en', 'lastname' => 'ag']);
+if($teacher2) {
+    foreach ($teacher2->getEmployeeList() as $entry) {
+        echo "Angestellt seit {$entry->getStartDate()->format("d.m.Y")}\n";
+    }
+    $em = new EntityManager();
+    $teacher2->setLastname('Wagner');
+    $em->persist($teacher2);
+    $em->flush();
+}
