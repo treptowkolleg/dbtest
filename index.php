ab157fe1398123f128b1f0e2b38aa7d89c8bfc51
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-12-16 19:13:00 +0100

    update

--- index.php
+++ index.php
@@ -2,7 +2,48 @@
 
 require 'vendor/autoload.php';
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
+
+/*
+ * Zuerst 'composer require treptowkolleg/orm' ausführen, um die Magic zu importieren. ;-)
+ */
+
+###########################################################
+# Tabellen anlegen
+
+/*
+ * Entity-Manager instantiieren zum Anlegen von Tabellen sowie speichern und aktualisieren von Datensätzen.
+ */
+$em = new \TreptowKolleg\ORM\Model\EntityManager();
+
+/*
+ * Dann Tabellen analog zu unseren Entity-Klassen anlegen.
+ */
+$em->createTable(Category::class);
+$em->createTable(Product::class);
+// usw.
+
+/*
+ * Neues Produkt erzeugen
+ */
+$myProduct = new Product();
+$myProduct->setLabel("Spielekonsole");
+
+/*
+ * Produkt persistent machen (festschreiben)
+ */
+$em->persist($myProduct);
+
+/*
+ * Alle bis hierher persistierten Objekte nun tatsächlich in die DB schreiben.
+ */
+$em->flush();
+
+
+###########################################################
+# Daten abrufen (Siehe auch src/Entity bzw. src/Repository)
 
 /*
  * Repositories enthalten allgemeine oder spezielle Methoden zum Finden von Datensätzen
@@ -11,7 +52,7 @@ use App\Repository\CategoryRepository;
 $repository = new CategoryRepository();
 
 /*
- * Hole Datensatz der ID 6 oder NULL
+ * Hole Datensatz mit der ID 6 (falls vorhanden, ansonsten NULL).
  */
 $category = $repository->find(6);
 
@@ -25,13 +66,3 @@ if($category){
         echo "$product\n";
     }
 }
-
-/*
- * Ausgabe:
- * * * * * * * * * * * * * * *
- * Kategorie Bildung enthält:
- * Malset
- * Konstruktionsspielzeug
- *
- */
-
