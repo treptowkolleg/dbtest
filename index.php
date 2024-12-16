0491791e1e24beae9c492aa3c30b4d4fbe520462
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-12-16 18:56:52 +0100

    update

--- index.php
+++ index.php
@@ -4,15 +4,34 @@ require 'vendor/autoload.php';
 
 use App\Repository\CategoryRepository;
 
+/*
+ * Repositories enthalten allgemeine oder spezielle Methoden zum Finden von Datensätzen
+ * nach bestimmten Kriterien.
+ */
 $repository = new CategoryRepository();
+
+/*
+ * Hole Datensatz der ID 6 oder NULL
+ */
 $category = $repository->find(6);
 
+/*
+ * Wenn $category nicht NULL, gib alle Produkte aus, die mit dieser Kategorie verknüpft sind.
+ */
 if($category){
     echo "Kategorie $category enthält:\n";
 
     foreach ($category->getProducts() as $product) {
-        echo "- $product\n";
+        echo "$product\n";
     }
 }
 
+/*
+ * Ausgabe:
+ * * * * * * * * * * * * * * *
+ * Kategorie Bildung enthält:
+ * Malset
+ * Konstruktionsspielzeug
+ *
+ */
 
